c03117dced0ae9bc5a7b7b5d2f0d449da8d30b8c
Heedong Yang <heedong@kaist.ac.kr>
2023-03-11 22:59:49 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,3 +2,4 @@
 mkdir -p workdir
 git clone https://github.com/B2R2-org/B2R2.git
 dotnet build -c Release ./B2R2
+wget https://github.com/ethereum/solidity/releases/download/v0.8.19/solc-static-linux
